4472246bc372ac5a5a543cd7060b18c45925ddd3
Gustavo Coronel <gcoronelc@gmail.com>
2021-04-16 19:03:05 -0500

    Reportes

--- JasperReport/Reportes.java
+++ JasperReport/Reportes.java
@@ -101,3 +101,77 @@ try {
 }
 
 
+// =======================================
+// Reportes Web
+// =======================================		
+
+// De WebContent\reportes\...
+
+try {
+	IArticuloDAO articuloDAO = DAOFactory.getDAOFactory().getArticuloDAO();
+	ArrayList<ArticuloTO> lista = articuloDAO.consultarPorNombre("%");
+	JRBeanCollectionDataSource data;
+	data = new JRBeanCollectionDataSource(lista);
+	ServletConfig config = getServletConfig();
+	ServletContext sc = config.getServletContext();
+	InputStream inputStream = sc.getResourceAsStream("/reportes/repoArticulos.jasper");
+	if (inputStream == null) { throw new Exception("Reporte no existe.<br>"); }
+	JasperReport jasperReport = (JasperReport) JRLoader.loadObject(inputStream);
+	byte[] bytes = JasperRunManager.runReportToPdf(jasperReport, null, data);
+	response.setContentType("application/pdf");
+	response.setContentLength(bytes.length);
+	ServletOutputStream out = response.getOutputStream();
+	out.write(bytes, 0, bytes.length);
+	out.flush();
+	out.close();
+} catch (Exception e) {
+	response.setContentType("text/html;charset=UTF-8");
+	PrintWriter out = response.getWriter();
+	out.println("Error: " + e.getMessage());
+	out.close();
+}
+
+
+// De /pe/eureka/reportes/
+
+try {
+	// Datos
+	String cuenta = request.getParameter("cuenta");
+	// Proceso
+	EurekaService service = new EurekaService();
+	List<Map<String, ?>> lista = service.getMovimientos(cuenta);
+	// Preparar reporte
+
+	JRMapCollectionDataSource data = new JRMapCollectionDataSource(lista);
+	Map pars = new HashMap<>();
+	
+	String reporte = "/pe/eureka/reportes/repoMovimientos.jasper";
+	//InputStream is = EurekaController.class.getResourceAsStream(reporte);
+	InputStream is = getClass().getResourceAsStream(reporte);
+	
+	/*
+	ServletConfig config = getServletConfig();
+	ServletContext sc = config.getServletContext();
+	String reporte = "/reportes/repoMovimientos.jasper";
+	InputStream is = sc.getResourceAsStream(reporte);
+	*/
+	if(is == null){
+		throw new Exception("Reporte no existe.");
+	}
+	
+	JasperReport rep = (JasperReport) JRLoader.loadObject(is);
+	byte[] bytes = JasperRunManager.runReportToPdf(rep, null, data);
+	// Preparar el reporte
+	response.setContentType("application/pdf");
+	response.setContentLengthLong(bytes.length);
+	ServletOutputStream out = response.getOutputStream();
+	out.write(bytes, 0, bytes.length);
+	out.flush();
+	out.close();
+} catch (Exception e) {
+	e.printStackTrace();
+	response.setContentType("text/html;charset=UTF-8");
+	PrintWriter out = response.getWriter();
+	out.println("Error: " + e.getMessage());
+	out.close();
+}
